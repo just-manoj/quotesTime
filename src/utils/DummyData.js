9fcc0148683a1befd88a91a63fd135301db364d4
infinity-bot-001 <infinity.bot.vision@gmail.com>
2022-08-27 20:29:59 +0530

    add Dummy Data

---
+++ src/utils/DummyData.js
@@ -0,0 +1,42 @@
+export const DummyData = [
+  {
+    quote: 'The purpose of our lives is to be happy',
+    name: 'Dalai Lama',
+    date: new Date(),
+  },
+  {
+    quote:
+      'Many of life’s failures are people who did not realize how close they were to success when they gave up',
+    name: 'Thomas A. Edison',
+    date: new Date(),
+  },
+  {
+    quote:
+      'If you want to live a happy life, tie it to a goal, not to people or things',
+    name: 'Albert Einstein',
+    date: new Date(),
+  },
+  {
+    quote:
+      'Money and success don’t change people; they merely amplify what is already there',
+    name: 'Will Smith',
+    date: new Date(),
+  },
+  {
+    quote:
+      'Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking',
+    name: 'Steve Jobs',
+    date: new Date(),
+  },
+  {
+    quote: 'You never really learn much from hearing yourself speak',
+    name: 'George Clooney',
+    date: new Date(),
+  },
+  {
+    quote:
+      'If something’s important enough, you should try. Even if - the probable outcome is failure',
+    name: 'Elon Musk',
+    date: new Date(),
+  },
+];
