fd1c3c63a8590e96b4eee06fda2e3b68086744b7
infinity-bot-001 <infinity.bot.vision@gmail.com>
2022-08-27 20:32:17 +0530

    adding two Screen

---
+++ src/screens/ManageQuote.js
@@ -0,0 +1,11 @@
+const { View, Text } = require('react-native');
+
+const ManageQuote = () => {
+  return (
+    <View>
+      <Text>ManageQuote Screen</Text>
+    </View>
+  );
+};
+
+export default ManageQuote;
---
+++ src/screens/QuotesList.js
@@ -0,0 +1,11 @@
+const { View, Text } = require('react-native');
+
+const QuotesList = () => {
+  return (
+    <View>
+      <Text>QuotesList Screen</Text>
+    </View>
+  );
+};
+
+export default QuotesList;
