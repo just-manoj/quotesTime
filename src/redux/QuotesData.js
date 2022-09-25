dc055262e4274f5ad7bd6570ac65417bf56448d7
infinity-bot-001 <infinity.bot.vision@gmail.com>
2022-09-25 20:25:53 +0530

    adding - sort quotes based on date functionality

--- src/redux/QuotesData.js
+++ src/redux/QuotesData.js
@@ -11,12 +11,27 @@ const QuotesData = createSlice({
     },
     setQuote: (state, action) => {
       action.payload.quotesDetails
-        .reverse()
+        .sort((a, b) => {
+          const c = new Date(a.date);
+          const d = new Date(b.date);
+          return d - c;
+        })
         .forEach((quote) => state.quotes.push(quote));
+    },
+    updateQuote: (state, action) => {
+      const index = state.quotes.findIndex(
+        (quote) => quote.id == action.payload.quoteData.id
+      );
+      const updatableQuote = state.quotes[index];
+      const quoteItem = { ...updatableQuote, ...action.payload.quoteData };
+      // state.quotes[index] = quoteItem;
+      state.quotes.splice(index, 1);
+      state.quotes.splice(0, 0, quoteItem);
     },
   },
 });
 
 export const addQuote = QuotesData.actions.addQuote;
 export const setQuote = QuotesData.actions.setQuote;
+export const updateQuote = QuotesData.actions.updateQuote;
 export default QuotesData.reducer;
